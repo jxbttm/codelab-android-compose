ce364c0d34028e5cf1a58e41d93a128cc08be000
jxbttm <su_jingxuan@hotmail.com>
2024-03-22 12:35:01 +0800

    practice: state lab done

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
@@ -29,7 +30,7 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun StatefulCounter(modifier: Modifier = Modifier) {
-    var count by rememberSaveable { mutableStateOf(0) }
+    var count by rememberSaveable { mutableIntStateOf(0) }
     StatelessCounter(
         count = count,
         onIncrement = { count++ },
